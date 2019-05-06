d662475ec99ef6cf38c617c01493ac64be7e3ac9
DESKTOP-BFU1N5B\gabi1 <gabrielle189189@gmail.com>
2019-05-06 07:55:11 -0300

    primeiro commit

---
+++ Unity--VR_monster/Assets/Scripts/ForminhasController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForminhasController : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void onGraspBegin()
+    {
+        animator.SetBool("isGrabbed", true);
+    }
+
+    public void onGraspEnd()
+    {
+        animator.SetBool("isGrabbed", false);
+    }
+}
