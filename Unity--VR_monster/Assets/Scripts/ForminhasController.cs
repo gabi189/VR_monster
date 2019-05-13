d3fcd314390d60676f326aeb1550378039536236
DESKTOP-BFU1N5B\gabi1 <gabrielle189189@gmail.com>
2019-05-13 16:09:21 -0300

    fim de semana

--- Unity--VR_monster/Assets/Scripts/ForminhasController.cs
+++ Unity--VR_monster/Assets/Scripts/ForminhasController.cs
@@ -15,13 +15,17 @@ public class ForminhasController : MonoBehaviour
 
     void Update()
     {
+        //descobrir se o game object está se movendo
+        // EEE se ele não está sendo grabbed 
+        //if (isGrabbed == false) ???
+        //animator.SetBool("isRunning", true); 
     }
 
     public void onGraspBegin()
     {
         animator.SetBool("isGrabbed", true);
         rigidbody.constraints = RigidbodyConstraints.None;
-        print("oi");
+       
     }
 
     public void onGraspEnd()
@@ -30,6 +34,8 @@ public class ForminhasController : MonoBehaviour
 
         rigidbody.constraints &= RigidbodyConstraints.FreezeRotationX;
         rigidbody.constraints &= RigidbodyConstraints.FreezeRotationZ;
-        print("tchau");
+        
     }
+
+
 }
